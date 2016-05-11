c3a22fdbd9fb548dd2a8830976659e11c3dec529
Kevin Thrailkill <kevinjt92@gmail.com>
2016-05-11 14:23:58 -0700

    Blocstagram checkpoint 31 completed

--- Blocstagram/Blocstagram/DataSource.h
+++ Blocstagram/Blocstagram/DataSource.h
@@ -7,12 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+@class Media;
 
 @interface DataSource : NSObject
 
 +(instancetype) sharedInstance;
 
 @property (nonatomic, strong, readonly) NSArray *mediaItems;
+
+ - (void) deleteMediaItem:(Media *)item;
 
 
 @end
