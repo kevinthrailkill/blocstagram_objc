afba9830cc7d4ed1f44627c6a05339f783344674
Kevin Thrailkill <kevinjt92@gmail.com>
2016-05-17 20:49:05 -0700

    Blocstagram - checkpoint 43 - posting image

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,16 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Kevin Thrailkill on 5/17/16.
+//  Copyright © 2016 kevinthrailkill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+
+@end
